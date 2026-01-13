2112bfcdf1810796fa2fb56602471bceaf47bc94
Sebastian Wintersperger <s.wintersperger@me.com>
2026-01-13 09:56:43 +0100

    fix: adjust some mobile landscape problems for motion clock

--- src/components/symbols/MotionSymbol.vue
+++ src/components/symbols/MotionSymbol.vue
@@ -1,8 +1,8 @@
 <!-- eslint-disable vue/component-name-in-template-casing -->
 <template>
   <motion.div
-    class="flex flex-col content-start max-h-screen will-change-transform" :animate="{ y }" :transition="{ type: 'spring', duration: 0.75, bounce: 0.5 }">
-    <span v-for="el in range" :key="el" :class="[el === props.symbol ? 'opacity-100' : 'opacity-10 sm:opacity-30']" class="flex justify-center items-center rounded font-semibold text-rose-800 text-5xl tracking-widest transition-opacity duration-250 select-none">
+    class="flex flex-col content-start will-change-transform" :animate="{ y }" :transition="{ type: 'spring', duration: 0.75, bounce: 0.5 }">
+    <span v-for="el in range" :key="el" :class="[el === props.symbol ? 'opacity-100' : 'opacity-10 sm:opacity-30']" class="flex justify-center items-center rounded font-semibold text-[48px] text-rose-800 leading-none tracking-widest transition-opacity duration-250 select-none">
       {{ Number(el) ? Number(el) : el }}
     </span>
   </motion.div>
